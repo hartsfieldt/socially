7f3f7e2e33bfeff0a2bb41ff9dfaf778fca60430
Your Name <you@example.com>
2022-12-08 21:02:47 -0500

    created thought and reaction schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,23 +1,67 @@
-const { Schema, model } = require("mongoose");
+const { DateTime } = require("luxon");
+const { Schema, model, Types } = require("mongoose");
 
-const ReactionSchema = new Schema({});
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      get: (createdAtVal) =>
+        DateTime.fromJSDate(createdAtVal).toLocaleString(DateTime.DATETIME_MED),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
 
-const ThoughtSchema = new Schema({
-  thoughtText: {
-    type: String,
-    required: true,
-    minlength: 1,
-    maxlength: 280,
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) =>
+        DateTime.fromJSDate(createdAtVal).toLocaleString(DateTime.DATETIME_MED),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [ReactionSchema],
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: (createdAtVal) =>
-      DateTime.fromJSDate(createdAtVal).toLocaleString(DateTime.DATETIME_MED),
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  reactions: [ReactionSchema],
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+      id: false,
+    },
+  }
+);
+
+ThoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
 });
+
+const Thought = model("Thought", ThoughtSchema);
+
+module.exports = Thought;
