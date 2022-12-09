6f0340c1c53e00e478494e99d50ceb31092a07f0
Your Name <you@example.com>
2022-12-08 21:17:37 -0500

    created a controller for thoughts to fetch information from the database

---
+++ controllers/thought-contoller.js
@@ -0,0 +1,14 @@
+const { Thought } = require("../models");
+
+const thoughtController = {
+  getAllThoughts(req, res) {
+    Thought.find({})
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+};
+
+module.exports = thoughtController;
